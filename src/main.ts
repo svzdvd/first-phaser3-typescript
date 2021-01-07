678f7e9049f6ca35f4fada0c6f8a7f14d38fd36b
Davide Savazzi <davide.savazzi@gmail.com>
2021-01-07 21:51:27 +0000

    first Phaser game in Typescript

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,15 @@ import Phaser from 'phaser'
 
 import HelloWorldScene from './scenes/HelloWorldScene'
 
-const config = {
+const config: Phaser.Types.Core.GameConfig = {
 	type: Phaser.AUTO,
 	width: 800,
 	height: 600,
 	physics: {
 		default: 'arcade',
 		arcade: {
-			gravity: { y: 200 }
+			gravity: { y: 300 },
+			debug: false
 		}
 	},
 	scene: [HelloWorldScene]
